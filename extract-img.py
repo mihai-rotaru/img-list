30b869a47dc5f9ecad88c73378c716d5e854fb8e
Mihai Rotaru <mihai_rotaru@lavabit.com>
2013-05-03 10:09:16 +0100

    initial commit

---
+++ extract-img.py
@@ -0,0 +1,27 @@
+from HTMLParser import HTMLParser
+import sys
+
+# from: http://stackoverflow.com/a/11659969/447661
+#-------------------------------------------------------------------------------
+def read_entirely( file ):
+    with open( file, 'r' ) as handle:
+        return handle.read()
+
+images = []
+
+# create a subclass and override the handler methods
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        if tag == 'img':
+            for name,value in attrs:
+                if name == 'src':
+                    images.append(value)
+
+# instantiate the parser and fed it some HTML
+parser = MyHTMLParser()
+f = read_entirely(sys.argv[1])
+parser.feed(f)
+
+out = open('out.txt','w')
+for item in images:
+  out.write("%s\n" % item)
